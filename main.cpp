395f131b39840dcfb8b69f01f65afe721acf120b
Javier Celaya <javier.celaya@flexvm.es>
2015-02-03 13:11:24 +0100

    Add a custom install script for Linux, and a service file

--- main.cpp
+++ main.cpp
@@ -3,12 +3,21 @@
  **/
 
 #include <iostream>
+#include <fstream>
+#include <sys/stat.h>
 #include "FlexVDIGuestAgent.hpp"
 #include "util.hpp"
 using namespace flexvm;
 using std::string;
 
 int main(int argc, char * argv[]) {
+    const char * logFileName = "/var/log/flexvdi_agent.log";
+    std::ofstream logFile(logFileName, std::ios_base::app);
+    logFile << std::endl << std::endl;
+    Log::setLogOstream(&logFile);
+    chmod(logFileName, S_IRUSR | S_IWUSR);
+    Log(L_INFO) << "Starting the flexVDI guest agent.";
+
     FlexVDIGuestAgent & agent = FlexVDIGuestAgent::singleton();
     for (int i = 1; i < argc; ++i) {
         if (string("-e") == argv[i] && ++i < argc) {
