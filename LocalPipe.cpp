45e01fff81129d63df2e5305916191daf7733380
Javier Celaya <javier.celaya@flexvm.es>
2015-02-03 15:54:14 +0100

    Refactor LocalPipe, put all platform-dependent code together

--- LocalPipe.cpp
+++ LocalPipe.cpp
@@ -27,10 +27,60 @@ struct LocalPipe::Listener {
     : stream(io) {}
     std::string pipeName;
     typedef asio::windows::stream_handle stream_t;
+
+    void reset() {
+        DisconnectNamedPipe(stream.native_handle());
+        stream.close();
+    }
+    template <typename H> void listen(H acceptHandler) {
+        SECURITY_ATTRIBUTES secAttr;
+        secAttr.nLength = sizeof(SECURITY_ATTRIBUTES);
+        secAttr.bInheritHandle = FALSE;
+        // Only current user may access the named pipe
+        secAttr.lpSecurityDescriptor = NULL;
+        uint32_t mode = PIPE_ACCESS_DUPLEX | FILE_FLAG_OVERLAPPED;
+        uint32_t type = PIPE_TYPE_BYTE | PIPE_READMODE_BYTE | PIPE_WAIT;
+        HANDLE pipeFd = ::CreateNamedPipeA(pipeName.c_str(), mode, type,
+                                           PIPE_UNLIMITED_INSTANCES, 1024, 1024, 0, &secAttr);
+        throw_if(pipeFd == INVALID_HANDLE_VALUE, pipeName);
+        stream.assign(pipeFd);
+        asio::windows::overlapped_ptr overlappedPtr;
+        overlappedPtr.reset(stream.get_io_service(), acceptHandler);
+        BOOL ok = ConnectNamedPipe(pipeFd, overlappedPtr.get());
+        throw_if(!ok && GetLastError() != ERROR_IO_PENDING, "ConnectNamedPipe failed");
+        // The operation was successfully initiated, so ownership of the
+        // OVERLAPPED-derived object has passed to the io_service.
+        overlappedPtr.release();
+    }
+    void open(const std::string & endpointName) {
+        pipeName = endpointName;
+        if (pipeName[0] != '\\')
+            pipeName = "\\\\.\\pipe\\" + pipeName;
+    }
+
 #else
     : acceptor(io), stream(io) {}
     asio::local::stream_protocol::acceptor acceptor;
     typedef asio::local::stream_protocol::socket stream_t;
+
+    void reset() {
+        stream.close();
+    }
+    template <typename H> void listen(H acceptHandler) {
+        acceptor.async_accept(stream, acceptHandler);
+    }
+    void open(const std::string & endpointName) {
+        std::string name = endpointName;
+        if (name[0] != '/')
+            name = "/tmp/" + name;
+        ::unlink(name.c_str());
+        // TODO: protect the access to the local socket
+        asio::local::stream_protocol::endpoint ep(name);
+        acceptor.open(ep.protocol());
+        acceptor.bind(ep);
+        acceptor.listen();
+    }
+
 #endif
     stream_t stream;
 };
@@ -68,57 +118,20 @@ void LocalPipe::open() {
         Log(L_ERROR) << "The local pipe endpoint is empty";
         return;
     }
-#ifdef WIN32
-    listener->pipeName = endpointName;
-    if (listener->pipeName[0] != '\\')
-        listener->pipeName = "\\\\.\\pipe\\" + listener->pipeName;
-#else
-    std::string name = endpointName;
-    if (name[0] != '/')
-        name = "/tmp/" + name;
-    ::unlink(name.c_str());
-    // TODO: protect the access to the local socket
-    asio::local::stream_protocol::endpoint ep(name);
-    listener->acceptor.open(ep.protocol());
-    listener->acceptor.bind(ep);
-    listener->acceptor.listen();
-#endif
+    listener->open(endpointName);
     listen();
 }
 
 
 void LocalPipe::listen() {
-    auto acceptHandler = std::bind(&LocalPipe::connectionAccepted, this, ph::_1);
-#ifdef WIN32
-    DisconnectNamedPipe(listener->stream.native_handle());
-    SECURITY_ATTRIBUTES secAttr;
-    secAttr.nLength = sizeof(SECURITY_ATTRIBUTES);
-    secAttr.bInheritHandle = FALSE;
-    // Only current user may access the named pipe
-    secAttr.lpSecurityDescriptor = NULL;
-    uint32_t mode = PIPE_ACCESS_DUPLEX | FILE_FLAG_OVERLAPPED;
-    uint32_t type = PIPE_TYPE_BYTE | PIPE_READMODE_BYTE | PIPE_WAIT;
-    HANDLE pipeFd = ::CreateNamedPipeA(listener->pipeName.c_str(), mode, type,
-                                       PIPE_UNLIMITED_INSTANCES, 1024, 1024, 0, &secAttr);
-    throw_if(pipeFd == INVALID_HANDLE_VALUE, listener->pipeName);
-    listener->stream.assign(pipeFd);
-    asio::windows::overlapped_ptr overlappedPtr;
-    overlappedPtr.reset(listener->stream.get_io_service(), acceptHandler);
-    BOOL ok = ConnectNamedPipe(pipeFd, overlappedPtr.get());
-    throw_if(!ok && GetLastError() != ERROR_IO_PENDING, "ConnectNamedPipe failed");
-    // The operation was successfully initiated, so ownership of the
-    // OVERLAPPED-derived object has passed to the io_service.
-    overlappedPtr.release();
-#else
-    listener->stream.close();
-    listener->acceptor.async_accept(listener->stream, acceptHandler);
-#endif
+    listener->listen(std::bind(&LocalPipe::connectionAccepted, this, ph::_1));
 }
 
 
 void LocalPipe::connectionAccepted(const error_code & error) {
     if (error) {
         Log(L_WARNING) << "Local connection failed: " << error.message();
+        listener->reset();
     } else {
         connections.emplace_back(new LocalConnection(listener->stream, handler));
         connections.back()->accepted();
