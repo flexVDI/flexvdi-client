ea981ea85bf44e298417db0d0cdc34c618f09474
Javier Celaya <javier.celaya@flexvm.es>
2015-02-13 09:40:40 +0100

    Test: Add unit test program

---
+++ test/unit-main.cpp
@@ -0,0 +1,14 @@
+/**
+ * Copyright Flexible Software Solutions S.L. 2014
+ **/
+
+#define BOOST_TEST_MODULE hdrmerge
+#include <boost/test/unit_test.hpp>
+#include "../util.hpp"
+#include <iostream>
+
+static int setLog() {
+    flexvm::Log::setLogOstream(&std::cout);
+    return 0;
+}
+static int foo = setLog();
