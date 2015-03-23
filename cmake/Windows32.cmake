63517515e655cc2297dfbc77148b58e8dcacac45
Javier Celaya <javier.celaya@flexvm.es>
2015-03-23 18:07:05 +0100

    Build: Improve discovery in Windows toolchain file

--- cmake/Windows32.cmake
+++ cmake/Windows32.cmake
@@ -1,25 +1,34 @@
 # Toolchain file to cross-compile for Windows 32-bit targets.
-# It is based on Fedoras's mingw-w64.
 
-# this one is important
 set(CMAKE_SYSTEM_NAME Windows)
 set(WIN_ARCH i686)
 set(WIN_BITS 32)
 
-# specify the cross compiler
 set(CMAKE_C_COMPILER   /usr/bin/${WIN_ARCH}-w64-mingw32-gcc)
 set(CMAKE_CXX_COMPILER /usr/bin/${WIN_ARCH}-w64-mingw32-g++)
 set(CMAKE_RC_COMPILER  /usr/bin/${WIN_ARCH}-w64-mingw32-windres)
 
-# here is the target environment located
-set(CMAKE_FIND_ROOT_PATH /usr/${WIN_ARCH}-w64-mingw32/sys-root/mingw)
+# Find the system root
+execute_process(COMMAND ${CMAKE_C_COMPILER} -print-sysroot
+                OUTPUT_VARIABLE MINGW_SYSROOT OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET)
+execute_process(COMMAND find "${MINGW_SYSROOT}" -name lib
+                OUTPUT_VARIABLE MINGW_SYSROOT OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET)
+string(REPLACE "/lib" "" MINGW_SYSROOT "${MINGW_SYSROOT}")
+set(CMAKE_FIND_ROOT_PATH
+    /usr/local/${WIN_ARCH}-w64-mingw32
+    "${MINGW_SYSROOT}")
 
-# adjust the default behaviour of the FIND_XXX() commands:
+# Adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search
 # programs in the host environment
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-# adjust paths for pkg-config
-set(ENV{PKG_CONFIG_LIBDIR} ${CMAKE_FIND_ROOT_PATH}/lib/pkgconfig:${CMAKE_FIND_ROOT_PATH}/share/pkgconfig)
+# Adjust paths for pkg-config
+set(ENV{PKG_CONFIG_LIBDIR} "")
+foreach (_DIR IN LISTS CMAKE_FIND_ROOT_PATH)
+    set(ENV{PKG_CONFIG_LIBDIR} "$ENV{PKG_CONFIG_LIBDIR}:${_DIR}/lib/pkgconfig")
+    set(ENV{PKG_CONFIG_LIBDIR} "$ENV{PKG_CONFIG_LIBDIR}:${_DIR}/share/pkgconfig")
+endforeach (_DIR)
+link_directories(/usr/local/${WIN_ARCH}-w64-mingw32/lib)
