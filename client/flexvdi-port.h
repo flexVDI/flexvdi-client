435c98bb5dbf98441f7fcd648a8510dc3ebcdf20
Javier Celaya <javier.celaya@flexvm.es>
2015-03-25 09:40:00 +0100

    Client: Add public client prototypes

---
+++ client/flexvdi-port.h
@@ -0,0 +1,21 @@
+/**
+ * Copyright Flexible Software Solutions S.L. 2014
+ **/
+
+#ifndef _FLEXVDI_PORT_H_
+#define _FLEXVDI_PORT_H_
+
+#include "spice-session.h"
+
+void flexvdi_port_register_session(SpiceSession *session);
+
+// SSO API
+void flexvdi_send_credentials(const gchar *username, const gchar *password,
+                              const gchar *domain);
+
+// Print Client API
+void flexvdi_get_printer_list(GSList ** printer_list);
+void flexvdi_share_printer(const char * printer);
+void flexvdi_unshare_printer(const char * printer);
+
+#endif /* _FLEXVDI_PORT_H_ */
