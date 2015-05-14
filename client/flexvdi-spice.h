d01b87edeaee4b6a32a861d87a62d48e0ea68190
Javier Celaya <javier.celaya@flexvm.es>
2015-05-14 13:05:37 +0200

    Build: define strnlen if it does not exist

--- client/flexvdi-spice.h
+++ client/flexvdi-spice.h
@@ -10,6 +10,16 @@
 #include <stdarg.h>
 #include <glib.h>
 #include <gio/gio.h>
+#include "config.h"
+
+// strnlen is not defined in MinGW32
+#ifndef HAVE_STRNLEN
+static __inline__ size_t strnlen(const char * start, size_t maxlen) {
+    const char * stop = start;
+    while (((stop - start) < maxlen) && *stop) ++stop;
+    return stop - start;
+}
+#endif
 
 typedef enum {
     L_DEBUG,
