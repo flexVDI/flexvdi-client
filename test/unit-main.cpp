43c39df74437346cb618bdbeedb39573d7f0267f
Javier Celaya <javier.celaya@flexvdi.com>
2015-06-11 15:05:39 +0200

    Test: Build static test modules

--- test/unit-main.cpp
+++ test/unit-main.cpp
@@ -2,13 +2,14 @@
  * Copyright Flexible Software Solutions S.L. 2014
  **/
 
-#define BOOST_TEST_MODULE flexvdi-guest-agent
 #include <boost/test/unit_test.hpp>
 #include "../util.hpp"
 #include <iostream>
+using namespace ::boost::unit_test;
 
-static int setLog() {
+test_suite* init_unit_test_suite(int, char * [])   {
     flexvm::Log::setLogOstream(&std::cout);
-    return 0;
+    assign_op(framework::master_test_suite().p_name.value,
+              BOOST_TEST_STRINGIZE(flexvdi-guest-agent), 0);
+    return nullptr;
 }
-static int foo = setLog();
