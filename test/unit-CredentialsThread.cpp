0f70705e16c9055ef77e8e49d748039fe1f14523
Javier Celaya <javier.celaya@flexvdi.com>
2015-06-12 09:00:41 +0200

    Test: Add CredentialsThread unit test

---
+++ test/unit-CredentialsThread.cpp
@@ -0,0 +1,100 @@
+/**
+ * Copyright Flexible Software Solutions S.L. 2014
+ **/
+
+#include <boost/test/unit_test.hpp>
+#include <boost/asio.hpp>
+#include "FlexVDIProto.h"
+#include "MessageBuffer.hpp"
+#include "LocalPipe.hpp"
+#include "util.hpp"
+#include "credentials-manager/CredentialsThread.hpp"
+using namespace std;
+namespace ph = std::placeholders;
+namespace asio = boost::asio;
+
+namespace flexvm {
+
+#define FIXTURE CredentialsThread_fixture
+struct FIXTURE : public CredentialsThread::Listener {
+    asio::io_service io;
+    CredentialsThread thread;
+    LocalPipe pipe;
+    Connection::Ptr conn;
+    MessageBuffer credentials;
+    string recvUser, recvPass, recvDom;
+    bool messageHandled, credentialsSent, credentialsReceived;
+    asio::deadline_timer timeout;
+
+#if defined(BOOST_ASIO_HAS_LOCAL_SOCKETS)
+    static constexpr const char * pipeName = "/tmp/flexvdi_pipe";
+#else
+    static constexpr const char * pipeName = "\\\\.\\pipe\\flexvdi_pipe";
+#endif
+
+    FIXTURE() : thread(*this), pipe(io, bind(&FIXTURE::handle, this, ph::_1, ph::_2)),
+                messageHandled(false), credentialsSent(false), credentialsReceived(false),
+                timeout(io, boost::posix_time::seconds(1)) {
+        pipe.setEndpoint(pipeName);
+        thread.setEndpoint(pipeName);
+        pipe.open();
+        credentials = sampleCredentials("user", "pass", "domi");
+        timeout.async_wait(bind(&FIXTURE::timedOut, this, ph::_1));
+    }
+
+    ~FIXTURE() {
+        ::unlink(pipeName);
+    }
+
+    void handle(const Connection::Ptr & src, const MessageBuffer & msg) {
+        BOOST_CHECK_EQUAL(msg.header().type, FLEXVDI_ASKCREDENTIALS);
+        BOOST_CHECK_EQUAL(msg.header().size, 0);
+        conn = src;
+        messageHandled = true;
+        conn->send(credentials, [this] { credentialsSent = true; });
+    }
+
+    virtual void credentialsChanged(const char * u, const char * p, const char * d) {
+        credentialsReceived = true;
+        recvUser = u;
+        recvPass = p;
+        recvDom = d;
+        io.stop();
+    }
+
+    static MessageBuffer sampleCredentials(const char * user, const char * pass, const char * dom) {
+        uint32_t userlen = strlen(user), passlen = strlen(pass), domlen = strlen(dom);
+        uint32_t bufSize = sizeof(FlexVDICredentialsMsg) + userlen + passlen + domlen + 3;
+        MessageBuffer result(FLEXVDI_CREDENTIALS, bufSize);
+        auto msg = result.getMessagePtr<FlexVDICredentialsMsg>();
+        msg->userLength = userlen;
+        msg->passLength = passlen;
+        msg->domainLength = domlen;
+        std::copy_n(user, userlen + 1, msg->strings);
+        std::copy_n(pass, passlen + 1, msg->strings + userlen + 1);
+        std::copy_n(dom, domlen + 1, msg->strings + userlen + passlen + 2);
+        return result;
+    }
+
+    void timedOut(const boost::system::error_code & error) {
+        if (!error)
+            io.stop();
+    }
+};
+
+
+BOOST_FIXTURE_TEST_CASE(CredentialsThread_testRead, FIXTURE) {
+    MessageBuffer credentials = sampleCredentials("user", "pass", "domi");
+    // TODO: Program timeout to stop io
+    thread.requestCredentials();
+    io.run();
+    io.reset();
+    BOOST_CHECK(messageHandled);
+    BOOST_CHECK(credentialsSent);
+    BOOST_CHECK(credentialsReceived);
+    BOOST_CHECK_EQUAL(recvUser, string("user"));
+    BOOST_CHECK_EQUAL(recvPass, string("pass"));
+    BOOST_CHECK_EQUAL(recvDom, string("domi"));
+}
+
+} // namespace flexvm
