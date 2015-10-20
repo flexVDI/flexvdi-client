0adacc224daf634e9c637468dbd1bd25222b8793
Javier Celaya <javier.celaya@flexvdi.com>
2015-10-20 11:08:23 +0200

    Build: Build Spice guest tools installer
    Build the Spice guest tools installer from latest VirtIO-Win iso and our
    own vdagent binaries.

--- find_win_components.sh
+++ find_win_components.sh
@@ -15,20 +15,7 @@ link_build_dir() {
         ln -s `dirname "$1"` win32
     fi
 }
-link_vdagent() {
-    if file "$1" | grep -q x86-64; then
-        echo "win64/vdagent.exe -> $1"
-        ln -s "$1" win64/vdagent.exe
-    else
-        echo "win32/vdagent.exe -> $1"
-        ln -s "$1" win32/vdagent.exe
-    fi
-}
-export -f link_build_dir link_vdagent
+export -f link_build_dir
 
 rm -f win32 win64
 find "$BASE" -name flexvdi-guest-agent.exe -type f -exec bash -c 'link_build_dir "$@"' bash \{\} \;
-
-rm -f win32/vdagent.exe win64/vdagent.exe
-find "$BASE" -name vdagent.exe -type f -exec bash -c 'link_vdagent "$@"' bash \{\} \;
-
