b09ba95e5cdf7c5c0422df31ca5ebfd6c10abe5c
Javier Celaya <javier.celaya@flexvdi.com>
2016-05-06 12:19:58 +0200

    Improve serialredir cmdline help

--- src/flexvdi-cmdline.c
+++ src/flexvdi-cmdline.c
@@ -20,14 +20,17 @@ const gchar ** getSerialPortParams() {
 static const GOptionEntry cmdline_entries[] = {
     // { long_name, short_name, flags, arg, arg_data, description, arg_description },
     { "flexvdi-serial-port", 0, 0, G_OPTION_ARG_STRING_ARRAY, &serialParams,
-      N_("Add serial port with parameters"), N_("<serial params>") },
+      "Add serial port redirection. "
+      "The Nth use of this option is attached to channel serialredirN. "
+      "Example: /dev/ttyS0,9600,8N1", "<device,speed,mode>" },
     { NULL, 0, 0, G_OPTION_ARG_NONE, NULL, NULL, NULL }
 };
 
 
 GOptionGroup * flexvdi_get_option_group(void) {
-    GOptionGroup * grp = g_option_group_new("flexvdi", _("flexVDI Options:"),
-                                            _("Show flexVDI Options"), NULL, NULL);
+    GOptionGroup * grp = g_option_group_new("flexvdi", "flexVDI Options:",
+                                            "Show flexVDI Options", NULL, NULL);
+    g_option_group_set_translation_domain(grp, "flexvdi");
     g_option_group_add_entries(grp, cmdline_entries);
 
     return grp;
