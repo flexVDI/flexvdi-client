fa45704a43eeb7a78d3d41627386d80162eb81ee
Javier Celaya <jcelaya@gmail.com>
2018-05-30 19:44:54 +0200

    Add a ClientRequest class

---
+++ src/client-request.c
@@ -0,0 +1,106 @@
+
+#include <string.h>
+#include <libsoup/soup.h>
+
+#include "client-request.h"
+
+struct _ClientRequest {
+    GObject parent;
+    SoupSession * soup;
+    GCancellable * cancel_mgr_request;
+    JsonParser * parser;
+    ClientRequestCallback cb;
+    gpointer user_data;
+    GError * error;
+};
+
+G_DEFINE_TYPE(ClientRequest, client_request, G_TYPE_OBJECT);
+
+static SoupSession * soup;
+
+static void client_request_init(ClientRequest * req) {
+    req->cancel_mgr_request = g_cancellable_new();
+}
+
+static void client_request_dispose(GObject * obj) {
+    ClientRequest * req = CLIENT_REQUEST(obj);
+    g_clear_object(&req->soup);
+    if (req->cancel_mgr_request)
+        g_cancellable_cancel(req->cancel_mgr_request);
+    g_clear_object(&req->cancel_mgr_request);
+    g_clear_object(&req->parser);
+    G_OBJECT_CLASS(client_request_parent_class)->dispose(obj);
+}
+
+static void client_request_finalize(GObject * obj) {
+    G_OBJECT_CLASS(client_request_parent_class)->finalize(obj);
+}
+
+static void client_request_class_init(ClientRequestClass * class) {
+    soup = soup_session_new();
+
+    GObjectClass * object_class = G_OBJECT_CLASS(class);
+    object_class->dispose = client_request_dispose;
+    object_class->finalize = client_request_finalize;
+}
+
+void client_request_cancel(ClientRequest * req) {
+    if (req->cancel_mgr_request) {
+        g_cancellable_cancel(req->cancel_mgr_request);
+        g_object_unref(req->cancel_mgr_request);
+        req->cancel_mgr_request = g_cancellable_new();
+    }
+}
+
+JsonNode * client_request_get_result(ClientRequest * req, GError ** error) {
+    if (error)
+        *error = req->error;
+    if (req->parser) {
+        return json_parser_get_root(req->parser);
+    } else {
+        return NULL;
+    }
+}
+
+static void request_parsed_cb(GObject * source_object, GAsyncResult * res, gpointer user_data) {
+    ClientRequest * req = CLIENT_REQUEST(user_data);
+    json_parser_load_from_stream_finish(req->parser, res, &req->error);
+    req->cb(req, req->user_data);
+}
+
+static void request_finished_cb(GObject * object, GAsyncResult * result, gpointer user_data) {
+	ClientRequest * req = CLIENT_REQUEST(user_data);
+	GInputStream * stream = soup_session_send_finish(SOUP_SESSION(object), result, &req->error);
+	if (req->error) {
+        req->cb(req, req->user_data);
+    } else {
+        req->parser = json_parser_new();
+        json_parser_load_from_stream_async(req->parser, stream, req->cancel_mgr_request,
+                                        request_parsed_cb, req);
+    }
+}
+
+ClientRequest * client_request_new(ClientConf * conf, const gchar * path,
+        ClientRequestCallback cb, gpointer user_data) {
+    ClientRequest * req = CLIENT_REQUEST(g_object_new(CLIENT_REQUEST_TYPE, NULL));
+    req->cb = cb;
+    req->user_data = user_data;
+    g_autofree gchar * uri = client_conf_get_connection_uri(conf, path);
+    SoupMessage * msg = soup_message_new("GET", uri);
+    soup_session_send_async(soup, msg, req->cancel_mgr_request,
+                            request_finished_cb, req);
+    return req;
+}
+
+ClientRequest * client_request_new_with_data(ClientConf * conf, const gchar * path,
+        const gchar * post_data, ClientRequestCallback cb, gpointer user_data) {
+    ClientRequest * req = CLIENT_REQUEST(g_object_new(CLIENT_REQUEST_TYPE, NULL));
+    req->cb = cb;
+    req->user_data = user_data;
+    g_autofree gchar * uri = client_conf_get_connection_uri(conf, path);
+    SoupMessage * msg = soup_message_new("POST", uri);
+    soup_message_set_request(msg, "text/json", SOUP_MEMORY_COPY, post_data, strlen(post_data));
+    soup_session_send_async(soup, msg, req->cancel_mgr_request,
+                            request_finished_cb, req);
+    return req;
+}
---
+++ src/client-request.h
@@ -0,0 +1,26 @@
+/**
+ * Copyright Flexible Software Solutions S.L. 2018
+ **/
+
+#ifndef _MANAGER_REQUEST_H
+#define _MANAGER_REQUEST_H
+
+#include <glib-object.h>
+#include <json-glib/json-glib.h>
+
+#include "configuration.h"
+
+
+#define CLIENT_REQUEST_TYPE (client_request_get_type())
+G_DECLARE_FINAL_TYPE(ClientRequest, client_request, CLIENT, REQUEST, GObject)
+
+typedef void (* ClientRequestCallback)(ClientRequest *, gpointer);
+ClientRequest * client_request_new(ClientConf * conf, const gchar * path,
+    ClientRequestCallback cb, gpointer user_data);
+ClientRequest * client_request_new_with_data(ClientConf * conf, const gchar * path,
+    const gchar * post_data, ClientRequestCallback cb, gpointer user_data);
+void client_request_cancel(ClientRequest * req);
+JsonNode * client_request_get_result(ClientRequest * req, GError ** error);
+
+
+#endif /* _MANAGER_REQUEST_H */
