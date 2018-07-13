d5ac8b7ad8a1bcba8d673df8d8ef7b146a563fe6
Javier Celaya <jcelaya@gmail.com>
2018-07-14 00:27:39 +0200

    Reorder client-win.c

--- src/client-win.c
+++ src/client-win.c
@@ -33,55 +33,7 @@ static guint signals[CLIENT_APP_LAST_SIGNAL];
 
 G_DEFINE_TYPE(ClientAppWindow, client_app_window, GTK_TYPE_APPLICATION_WINDOW);
 
-static void save_config(ClientAppWindow * win);
-
-static void button_pressed_handler(GtkButton * button, gpointer user_data) {
-    ClientAppWindow * win = CLIENT_APP_WINDOW(user_data);
-    if (button == win->config)
-        g_signal_emit(win, signals[CLIENT_APP_CONFIG_BUTTON_PRESSED], 0);
-    else if (button == win->save) {
-        save_config(win);
-        g_signal_emit(win, signals[CLIENT_APP_SAVE_BUTTON_PRESSED], 0);
-    } else if (button == win->login)
-        g_signal_emit(win, signals[CLIENT_APP_LOGIN_BUTTON_PRESSED], 0);
-}
-
-static void entry_activate_handler(GtkEntry * entry, gpointer user_data) {
-    ClientAppWindow * win = CLIENT_APP_WINDOW(user_data);
-    if (entry == win->password)
-        g_signal_emit(win, signals[CLIENT_APP_LOGIN_BUTTON_PRESSED], 0);
-}
-
-static void desktop_selected_handler(GtkTreeView * tree_view, GtkTreePath * path,
-                                     GtkTreeViewColumn * column, gpointer user_data) {
-    ClientAppWindow * win = CLIENT_APP_WINDOW(user_data);
-    g_signal_emit(win, signals[CLIENT_APP_DESKTOP_SELECTED], 0);
-}
-
-static void client_app_window_init(ClientAppWindow * win) {
-    GtkCssProvider * css_provider = gtk_css_provider_new();
-    gtk_css_provider_load_from_resource(css_provider, "/com/flexvdi/client/style.css");
-    GdkScreen * screen = gtk_widget_get_screen(GTK_WIDGET(win));
-    gtk_style_context_add_provider_for_screen(screen, GTK_STYLE_PROVIDER(css_provider),
-                                              GTK_STYLE_PROVIDER_PRIORITY_USER + 1);
-    gtk_widget_init_template(GTK_WIDGET(win));
-
-    gtk_label_set_text(win->version, "version " VERSION_STRING);
-    g_signal_connect(win->config, "clicked", G_CALLBACK(button_pressed_handler), win);
-    g_signal_connect(win->save, "clicked", G_CALLBACK(button_pressed_handler), win);
-    g_signal_connect(win->login, "clicked", G_CALLBACK(button_pressed_handler), win);
-    g_signal_connect(win->password, "activate", G_CALLBACK(entry_activate_handler), win);
-    win->desk_store = gtk_list_store_new(1, G_TYPE_STRING);
-    gtk_tree_view_set_model(win->desktops, GTK_TREE_MODEL(win->desk_store));
-    g_signal_connect(win->desktops, "row-activated", G_CALLBACK(desktop_selected_handler), win);
-}
-
-static void client_app_window_dispose(GObject * obj) {
-    ClientAppWindow * win = CLIENT_APP_WINDOW(obj);
-    g_clear_object(&win->conf);
-    g_clear_object(&win->desk_store);
-    G_OBJECT_CLASS(client_app_window_parent_class)->dispose(obj);
-}
+static void client_app_window_dispose(GObject * obj);
 
 static void client_app_window_class_init(ClientAppWindowClass * class) {
     G_OBJECT_CLASS(class)->dispose = client_app_window_dispose;
@@ -140,6 +92,61 @@ static void client_app_window_class_init(ClientAppWindowClass * class) {
                      g_cclosure_marshal_VOID__VOID,
                      G_TYPE_NONE,
                      0);
+}
+
+static void button_pressed_handler(GtkButton * button, gpointer user_data);
+static void entry_activate_handler(GtkEntry * entry, gpointer user_data);
+static void desktop_selected_handler(GtkTreeView * tree_view, GtkTreePath * path,
+                                     GtkTreeViewColumn * column, gpointer user_data);
+
+static void client_app_window_init(ClientAppWindow * win) {
+    GtkCssProvider * css_provider = gtk_css_provider_new();
+    gtk_css_provider_load_from_resource(css_provider, "/com/flexvdi/client/style.css");
+    GdkScreen * screen = gtk_widget_get_screen(GTK_WIDGET(win));
+    gtk_style_context_add_provider_for_screen(screen, GTK_STYLE_PROVIDER(css_provider),
+                                              GTK_STYLE_PROVIDER_PRIORITY_USER + 1);
+    gtk_widget_init_template(GTK_WIDGET(win));
+
+    gtk_label_set_text(win->version, "version " VERSION_STRING);
+    g_signal_connect(win->config, "clicked", G_CALLBACK(button_pressed_handler), win);
+    g_signal_connect(win->save, "clicked", G_CALLBACK(button_pressed_handler), win);
+    g_signal_connect(win->login, "clicked", G_CALLBACK(button_pressed_handler), win);
+    g_signal_connect(win->password, "activate", G_CALLBACK(entry_activate_handler), win);
+    win->desk_store = gtk_list_store_new(1, G_TYPE_STRING);
+    gtk_tree_view_set_model(win->desktops, GTK_TREE_MODEL(win->desk_store));
+    g_signal_connect(win->desktops, "row-activated", G_CALLBACK(desktop_selected_handler), win);
+}
+
+static void client_app_window_dispose(GObject * obj) {
+    ClientAppWindow * win = CLIENT_APP_WINDOW(obj);
+    g_clear_object(&win->conf);
+    g_clear_object(&win->desk_store);
+    G_OBJECT_CLASS(client_app_window_parent_class)->dispose(obj);
+}
+
+static void save_config(ClientAppWindow * win);
+
+static void button_pressed_handler(GtkButton * button, gpointer user_data) {
+    ClientAppWindow * win = CLIENT_APP_WINDOW(user_data);
+    if (button == win->config)
+        g_signal_emit(win, signals[CLIENT_APP_CONFIG_BUTTON_PRESSED], 0);
+    else if (button == win->save) {
+        save_config(win);
+        g_signal_emit(win, signals[CLIENT_APP_SAVE_BUTTON_PRESSED], 0);
+    } else if (button == win->login)
+        g_signal_emit(win, signals[CLIENT_APP_LOGIN_BUTTON_PRESSED], 0);
+}
+
+static void entry_activate_handler(GtkEntry * entry, gpointer user_data) {
+    ClientAppWindow * win = CLIENT_APP_WINDOW(user_data);
+    if (entry == win->password)
+        g_signal_emit(win, signals[CLIENT_APP_LOGIN_BUTTON_PRESSED], 0);
+}
+
+static void desktop_selected_handler(GtkTreeView * tree_view, GtkTreePath * path,
+                                     GtkTreeViewColumn * column, gpointer user_data) {
+    ClientAppWindow * win = CLIENT_APP_WINDOW(user_data);
+    g_signal_emit(win, signals[CLIENT_APP_DESKTOP_SELECTED], 0);
 }
 
 static void load_config(ClientAppWindow * win) {
