18bddc47caa34c7fe9a708abef1f0b2a47a23567
Javier Celaya <javier.celaya@flexvdi.com>
2018-08-30 14:59:38 +0200

    Disable client-side decorations on Windows

--- src/main.c
+++ src/main.c
@@ -3,6 +3,10 @@
 #include "client-app.h"
 
 int main (int argc, char * argv[]) {
+#ifdef WIN32
+    g_setenv("GTK_CSD", "0", TRUE);
+#endif
+
     gst_init(&argc, &argv);
     return g_application_run(G_APPLICATION(client_app_new()), argc, argv);
 }
