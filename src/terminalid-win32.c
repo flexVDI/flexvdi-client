5ee528ef27a8ef884c2d9758b86185617d1394c4
Javier Celaya <jcelaya@gmail.com>
2018-08-30 16:44:49 +0200

    Refactor terminalid-win32

--- src/terminalid-win32.c
+++ src/terminalid-win32.c
@@ -7,20 +7,17 @@
 #include <glib.h>
 
 gchar * discover_terminal_id() {
-    DWORD dwBufLen = 0;
-    gchar * result = NULL;
-    GetAdaptersInfo(NULL, &dwBufLen);
-    PIP_ADAPTER_INFO adapters = g_malloc(dwBufLen), it;
-    if (GetAdaptersInfo(adapters, &dwBufLen) == ERROR_SUCCESS) {
+    DWORD buf_len = 0;
+    GetAdaptersInfo(NULL, &buf_len);
+    g_autofree PIP_ADAPTER_INFO adapters = g_malloc(buf_len), it;
+    if (GetAdaptersInfo(adapters, &buf_len) == ERROR_SUCCESS) {
         for (it = adapters; it != NULL; it = it->Next) {
             if (it->AddressLength >= 6) {
-                result = g_strdup_printf("%02x:%02x:%02x:%02x:%02x:%02x",
+                return g_strdup_printf("%02x:%02x:%02x:%02x:%02x:%02x",
                     it->Address[0], it->Address[1], it->Address[2],
                     it->Address[3], it->Address[4], it->Address[5]);
-                break;
             }
         }
-        g_free(adapters);
     }
-    return result ? result : g_strdup("");
+    return g_strdup("");
 }
