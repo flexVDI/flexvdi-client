143491085ce610dc04ff706c424775f6bcb675be
Javier Celaya <jcelaya@gmail.com>
2018-11-15 18:57:07 +0100

    Add copyright/license notes to all source files

--- src/client-win.h
+++ src/client-win.h
@@ -1,3 +1,22 @@
+/*
+    Copyright (C) 2014-2018 Flexible Software Solutions S.L.U.
+
+    This file is part of flexVDI Client.
+
+    flexVDI Client is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    flexVDI Client is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with flexVDI Client. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #ifndef _CLIENT_WIN_H
 #define _CLIENT_WIN_H
 
