ac0e737f6c23931b25d5e6863c0d88847112cf25
Javier Celaya <javier.celaya@flexvdi.com>
2019-03-15 13:04:05 +0100

    FLEXVDI-3537 Add debug logs to terminalid-linux.c

--- src/terminalid-linux.c
+++ src/terminalid-linux.c
@@ -21,24 +21,38 @@
 #include <net/if.h>
 #include <unistd.h>
 #include <netinet/in.h>
+#include <errno.h>
 #include <glib.h>
 
+#ifdef G_LOG_DOMAIN
+#undef G_LOG_DOMAIN
+#endif
+#define G_LOG_DOMAIN "flexvdi-tid"
+
 gchar * discover_terminal_id() {
+    g_debug("Discovering terminal ID");
     gchar * id = NULL;
     struct ifreq ifr;
 
     int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
     struct if_nameindex * if_ni = if_nameindex(), * i;
+    g_debug("socket returned %d, if_nameindex returned %p", sock, if_ni);
     if (sock != -1 && if_ni != NULL) {
         for (i = if_ni; i->if_index != 0 && i->if_name != NULL; ++i) {
+            g_debug("Checking interface %s", i->if_name);
             memcpy(ifr.ifr_name, i->if_name, strlen(i->if_name));
-            if (ioctl(sock, SIOCGIFFLAGS, &ifr) == 0
-                && !(ifr.ifr_flags & IFF_LOOPBACK) // don't count loopback
-                && ioctl(sock, SIOCGIFHWADDR, &ifr) == 0) {
+            if (ioctl(sock, SIOCGIFFLAGS, &ifr) != 0) {
+                g_debug("SIOCGIFFLAGS ioctl failed: %s", strerror(errno));
+            } else if (ifr.ifr_flags & IFF_LOOPBACK) {
+                g_debug("Skipping the loopback device");
+            } else if (ioctl(sock, SIOCGIFHWADDR, &ifr) != 0) {
+                g_debug("SIOCGIFHWADDR ioctl failed: %s", strerror(errno));
+            } else {
                 close(sock);
                 unsigned char * mac = (unsigned char *)ifr.ifr_hwaddr.sa_data;
                 id = g_strdup_printf("%02x:%02x:%02x:%02x:%02x:%02x",
                     mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+                g_debug("Its MAC address is %s", id);
                 break;
             }
         }
