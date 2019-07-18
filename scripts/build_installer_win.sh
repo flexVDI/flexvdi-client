e956978fa5b44d423565d3e37704da09df7c8a3c
Javier Celaya <javier.celaya@flexvdi.com>
2019-07-18 09:26:23 +0200

    Win-Installer: Get compiler&tools from environment
    If CC is defined, use it to get the machine string instead of assuming
    its value. This is needed because the last Cerbero toolchain is multilib
    and the machine string is always x86_64-w64-mingw32. Then, calculate
    tool names after the machine string if they are not defined in the
    environment already.

--- scripts/build_installer_win.sh
+++ scripts/build_installer_win.sh
@@ -19,11 +19,19 @@
 
 PREFIX="$1"
 BUILD_TYPE="$2"
-if file src/flexvdi-client.exe | grep -q x86-64; then
-    HOST=x86_64-w64-mingw32
+
+if [ -z "$CC" ]; then
+    if file src/flexvdi-client.exe | grep -q x86-64; then
+        HOST=x86_64-w64-mingw32
+    else
+        HOST=i686-w64-mingw32
+    fi
+    CC=${HOST}-gcc
 else
-    HOST=i686-w64-mingw32
+    HOST=$($CC -dumpmachine)
 fi
+[ -z "$STRIP" ] && STRIP=${HOST:+$HOST-}strip
+[ -z "$OBJDUMP" ] && OBJDUMP=${HOST:+$HOST-}objdump
 SYSTEM_DLLS="kernel32.dll user32.dll msvcrt.dll gdi32.dll msimg32.dll advapi32.dll \
     ole32.dll shell32.dll ws2_32.dll dnsapi.dll iphlpapi.dll usp10.dll dwmapi.dll \
     imm32.dll setupapi.dll winmm.dll comctl32.dll comdlg32.dll imm32.dll crypt32.dll \
@@ -31,7 +39,7 @@ SYSTEM_DLLS="kernel32.dll user32.dll msvcrt.dll gdi32.dll msimg32.dll advapi32.d
 
 
 walk_dlls() {
-    ${HOST}-objdump -p "$1" | grep -i '\.dll$' | sed 's/.*\s\(\S\S*\)/\1/' | while read dll_name; do
+    $OBJDUMP -p "$1" | grep -i '\.dll$' | sed 's/.*\s\(\S\S*\)/\1/' | while read dll_name; do
         local dll_path=$(find "${DLL_DIRS[@]}" -maxdepth 1 -iname "$dll_name" -print -quit)
         if [ -z "$dll_path" -o ! -f "$dll_path" ]; then
             echo $SYSTEM_DLLS | grep -iq "$dll_name" || echo "$dll_name ($dll_path) not found"
@@ -52,14 +60,14 @@ cp "$PREFIX"/lib/gio/modules/libgiognutls.dll output/lib/gio/modules
 cp "$PREFIX"/lib/gstreamer-1.0/libgst{app,coreelements,audioconvert,audioresample,autodetect,playback,jpeg,videofilter,videoconvert,videoscale,deinterlace,directsound}.dll output//lib/gstreamer-1.0
 
 # Find DLLs
-DLL_DIRS=( $(find "$PREFIX" $(${HOST}-gcc -print-sysroot) -iname "*.dll" -type f -exec dirname \{\} \; 2>/dev/null | sort -u) )
+DLL_DIRS=( $(find "$PREFIX" $($CC -print-sysroot) -iname "*.dll" -type f -exec dirname \{\} \; 2>/dev/null | sort -u) )
 for bin in $(find output -type f) ; do
     walk_dlls "$bin"
 done
 
 # strip binaries
 if [ "$BUILD_TYPE" != "Debug" ]; then
-    find output \( -iname "*.dll" -o -iname "*.exe" \) -a -exec ${HOST}-strip \{\} + >& /dev/null
+    find output \( -iname "*.dll" -o -iname "*.exe" \) -a -exec $STRIP \{\} + >& /dev/null
 fi
 
 cp "$PREFIX"/bin/usb.ids output/bin
